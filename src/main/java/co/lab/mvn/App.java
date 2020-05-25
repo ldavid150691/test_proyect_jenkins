a14a5ae8d97eed17d48df86fa20e5e0864367379
ldavid150691 <40843415+ldavid150691@users.noreply.github.com>
2020-05-25 18:53:54 -0500

    Commit bug fix

--- src/main/java/co/lab/mvn/App.java
+++ src/main/java/co/lab/mvn/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        SSystem.out.println( "Hello World from jenkins lab" );
+        System.out.println( "Hello World from jenkins lab" );
     }
 }
